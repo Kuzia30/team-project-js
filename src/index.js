ea191be2c6103fa8637ba056b0ceecf22408ccdb
Eugenia-d <e.drobiazko@gmail.com>
2021-12-03 23:09:09 +0200

    [FT-07] fetching popular movies

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './sass/main.scss';
-import './js/showFilms'
+import './js/showFilms';
+
+// import { fetchMovies } from './js/API/theMovieApi';
+
+// fetchMovies().then(data => {
+//   console.log(data);
+// });
---
+++ src/js/API/theMovieApi.js
@@ -0,0 +1,7 @@
+const API_KEY = 'd28eeb5f1e0206ee46e3ed30e2f4030f';
+const API_URL = 'https://api.themoviedb.org/3';
+const API_ENDPOINT_MOVIES = '/discover/movie?sort_by=popularity.desc';
+
+export const fetchMovies = (page = 1) => {
+  return fetch(`${API_URL}${API_ENDPOINT_MOVIES}&api_key=${API_KEY}&page=${page}`);
+};
