0107d1344629a66ae68d50d86ee1f729213cf7d7
AnastasiiaBrd <nastuxa1357@gmail.com>
2021-12-03 23:01:40 +0200

    spinner

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './sass/main.scss';
-import './js/showFilms'
+import './js/showFilms';
+
+const loader = document.querySelector(`.loader`);
+window.addEventListener(`load`, stopScrolling);
+
+function stopScrolling() {
+  loader.classList.add(`slow`);
+  loader.style.display = `none`;
+}
