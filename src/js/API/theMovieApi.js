28b776bd4c378bee7d29d87b7d34afd302e360f4
Kuzia30 <maxzimka37@gmail.com>
2021-12-04 16:14:27 +0200

    fix Api

--- src/js/API/theMovieApi.js
+++ src/js/API/theMovieApi.js
@@ -3,5 +3,12 @@ const API_URL = 'https://api.themoviedb.org/3';
 const API_ENDPOINT_MOVIES = '/discover/movie?sort_by=popularity.desc';
 
 export const fetchMovies = (page = 1) => {
-  return fetch(`${API_URL}${API_ENDPOINT_MOVIES}&api_key=${API_KEY}&page=${page}`);
-};
+  return fetch(`${API_URL}${API_ENDPOINT_MOVIES}&api_key=${API_KEY}&page=${page}`)
+    .then(response => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+})
+}
+
