9dfcbea0b8c947cbcf8a9b912ecba0a8a5b655cd
Svit-l <lov.svit@gmail.com>
2021-12-05 02:54:20 +0200

    details-styles-start

---
+++ src/js/details.js
@@ -0,0 +1,16 @@
+import fetchMovies from './js/API/fetchMovies';
+
+// export function renderMovies(inputData) {
+//   return fetch(`${BASE_URL}/name/${inputData}?fields=name,capital,population,flags,languages`).then(
+//     res => {
+//       if (res.ok) {
+//         return res.json();
+//       }
+//       throw Error(res.statusText);
+//     },
+//   );
+// }
+
+//  https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d28eeb5f1e0206ee46e3ed30e2f4030f&page=1
+//
+// 8961361d6e18d951b539:4994c4436b9c770ce4b443f64dd963999e9912e43f11237a5731c7acbebe627b
