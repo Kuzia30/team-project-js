87b8fe034f3856d708c5da560e2218084b1e9568
AnastasiiaBrd <nastuxa1357@gmail.com>
2021-12-05 16:49:48 +0200

    data-atribut-and-new-partials

---
+++ src/js/renderImages.js
@@ -0,0 +1,35 @@
+import { objGanres } from '/js/ganres';
+
+const gallery = document.querySelector(`.gallery`);
+
+export function renderImages(results) {
+  const markup = results
+    .map(({ poster_path, id, title, genre_ids, release_date }) => {
+      const full_path = 'https://image.tmdb.org/t/p/w500/' + poster_path;
+      const date = new Date(release_date);
+      const yearRelease = date.getFullYear();
+      let genres = genre_ids.map(id => {
+        return objGanres[id];
+      });
+      if (genres.length > 3) {
+        const longGenresList = genres.splice(0, 2);
+        genres = longGenresList.join(', ') + ', Other';
+      } else {
+        genres = genres.join(', ');
+      }
+
+      return `<div class="gallery__item" data-id="${id}">
+          <img class="image-card" src="${full_path}" alt="${title}" loading="lazy"/>
+          <div class="info">
+    <p class="info-title">
+      ${title.toUpperCase()}
+    </p>
+    <p class="info-item">
+      ${genres} <span>|</span> ${yearRelease}
+    </p>
+  </div></div>`;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
