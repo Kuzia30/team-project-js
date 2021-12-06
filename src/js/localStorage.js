3be6cff1859f6b5a2d11c1e3436991e9c6df1b23
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2021-12-06 06:57:06 +0200

    add localStorage cardlib
    Сделал отрисовку карточек в библиотеке через hbs, добавил свой запрос отрисовки без рейтинга, изменены стили

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,32 +1,109 @@
+import { fetchMovie } from './API/theMovieApi';
 
-// Принимает ключ `key` по которому выбираем обьект.
-const load = key => {
-  try {
-    let serialState = localStorage.getItem(key);
+export default class LocalStorService {
+  constructor() {
+    if (JSON.parse(localStorage.getItem('Watched'))) {
+      let watchedArray = JSON.parse(localStorage.getItem('Watched'));
+      this.watchedArray = watchedArray;
+    } else {
+      this.watchedArray = [];
+    }
 
-    return (serialState = JSON.parse(serialState) || undefined);
-  } catch (err) {
-    console.error('Get state error: ', err);
+    if (JSON.parse(localStorage.getItem('Queue'))) {
+      let queueArray = JSON.parse(localStorage.getItem('Queue'));
+      this.queueArray = queueArray;
+    } else {
+      this.queueArray = [];
+    }
+
+    this.id = 0;
   }
-};
 
-// Сохраняем ключ `key` и значение `value`.
-const save = (key, value) => {
-  try {
-    const serialState = JSON.stringify(value);
-    localStorage.setItem(key, serialState);
-  } catch (err) {
-    console.error('Set state error: ', err);
+  set currentID(movieId) {
+    return (this.id = movieId);
   }
-};
 
-// Удаляем ключ `key`
-const remove = key => {
-  try {
-    localStorage.removeItem(key);
-  } catch (err) {
-    console.error('Remove state error: ', err);
+  get currentID() {
+    return this.id;
   }
-};
 
-export { load, save, remove };
+  // добавить фильм в Watched
+  async setWatchedToStorage() {
+    const object = await fetchMovie(this.id);
+
+    const info = renderImages(object);
+
+    this.watchedArray.unshift(info);
+    try {
+      localStorage.setItem('Watched', JSON.stringify(this.watchedArray));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  //Удалить фильм из списка просмотренных
+  delFromWatched() {
+    let array = this.getFromWatchedLS();
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].id === this.id) {
+        array.splice(i, 1);
+      }
+    }
+    this.watchedArray = array;
+    try {
+      localStorage.setItem('Watched', JSON.stringify(this.watchedArray));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // добавить фильм в Queue
+  async setQueueToStorage() {
+    const array = await fetchMovie(this.id);
+    const info = renderImages(array);
+    this.queueArray.unshift(info);
+    try {
+      localStorage.setItem('Queue', JSON.stringify(this.queueArray));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  //Удалить фильм из списка очереди
+  delFromQueue() {
+    let array = this.getQueueLS();
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].id === this.id) {
+        array.splice(i, 1);
+      }
+    }
+    this.queueArray = array;
+    try {
+      localStorage.setItem('Queue', JSON.stringify(this.queueArray));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // Получить массив объектов из списка Watched
+  getFromWatchedLS() {
+    try {
+      const movieArr = JSON.parse(localStorage.getItem('Watched'));
+      return movieArr === null ? undefined : movieArr;
+    } catch (err) {
+      console.error('Get state error: ', err);
+    }
+  }
+
+  // Получить массив объектов из списка Queue
+  getQueueLS() {
+    try {
+      const movieArr = JSON.parse(localStorage.getItem('Queue'));
+      return movieArr === null ? undefined : movieArr;
+    } catch (err) {
+      console.error('Get state error: ', err);
+    }
+  }
+}
