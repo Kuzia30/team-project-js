40b4849e3837c53bf819374a198427590ff7f515
Svit-l <lov.svit@gmail.com>
2021-12-06 08:22:58 +0200

    pop-up about movie render

--- src/js/details.js
+++ src/js/details.js
@@ -1,4 +1,92 @@
-import fetchMovies from './js/API/fetchMovies';
+// import { fetchMovies } from './API/theMovieApi';
+// import refs from './js/refs';
+
+// console.log(refs.closeBtn);
+// const closeBtn = document.querySelector('.pop-up__close-btn');
+
+// refs.closeBtn.addEventListener('click', closeMovieWindow);
+
+// function closeMovieWindow() {
+//   refs.backdrop.classList.add('visually-hidden');
+// }
+// import { fetchMovie } from './API/theMovieApi';
+// import refs from './refs';
+
+// import { square, diag } from './renderImages';
+
+// console.log(square(5), diag(6, 6));
+
+// const basicLightbox = require('basiclightbox');
+// import * as basicLightbox from 'basiclightbox';
+
+// const gallery = document.querySelector('.gallery');
+// console.log(gallery);
+// const popup = document.querySelector('.backdrop');
+
+// console.log(popup);
+
+// gallery.addEventListener('click', selectMovie);
+// // window.addEventListener('keydown', closeEscape);
+
+// function selectMovie(event) {
+//   //   event.preventDefault();
+//   const selectedFilm = event.carget.dataset.id;
+//   //   console.log(selectedFilm);
+//   const instant = basicLightbox.create(popup);
+//   instant.show();
+// }
+
+// function closeEscape(event) {
+//   const instance = basicLightbox.create(popup);
+//   if (event.key === 'Escape') {
+//     // console.log(instance);
+//     instance.close(() => console.log('lightbox not visible anymore'));
+
+//     return;
+//   }
+// }
+
+// const instance = basicLightbox.create('.backdrop', {
+//   onShow: instance => {
+//     instance
+//       .element()
+//       .querySelector('.backdrop')
+//       .addEventListener('click', instance.show(), { once: true });
+//   },
+
+//   // console.log('onShow', instance),
+//   //   onClose: instance => console.log('onClose', instance),
+// });
+// document.querySelector('.gallery').onclick = () => {
+//   basicLightbox.create('.backdrop').show();
+// };
+
+// document.querySelector('.pop-up__close-btn').onclick = () => {
+//   basicLightbox.create('.backdrop').show();
+// };
+// gallery.addEventListener('click', selectMovie);
+
+// function selectMovie(event) {
+//   event.preventDefault();
+
+//   console.log('hello');
+
+//   instance.show();
+// }
+
+// function closePopup(event) {
+//   console.log(event, 'buy');
+//   instance.close();
+// }
+
+// const instance = basicLightbox.create(document.querySelector('.backdrop'), {
+//   onShow: () => {
+//     gallery.addEventListener('click', selectMovie);
+//   },
+//   onClose: () => {
+//     window.removeEventListener('keydown', closePopup);
+//   },
+// });
 
 // export function renderMovies(inputData) {
 //   return fetch(`${BASE_URL}/name/${inputData}?fields=name,capital,population,flags,languages`).then(
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,12 +1,16 @@
 export default {
-    //gallery
-    gallery: document.querySelector('.gallery'),
-    searchInput: document.querySelector('.input-key'),
-    //preloader
-    preloader: document.querySelector('.loader'),
-    //modal
-    modal: document.querySelector('.modal'),
-    //library-header
-    watchedBtn: document.querySelector('.watched-js'),
-    queueBtn: document.querySelector('.queue-js'),
+  //gallery
+  gallery: document.querySelector('.gallery'),
+  searchInput: document.querySelector('.input-key'),
+  //preloader
+  preloader: document.querySelector('.loader'),
+  //modal
+  modal: document.querySelector('.modal'),
+  //library-header
+  watchedBtn: document.querySelector('.watched-js'),
+  queueBtn: document.querySelector('.queue-js'),
+  // pop-up about film
+  backdrop: document.querySelector('.backdrop'),
+  closeBtn: document.querySelector('.pop-up__close-btn'),
+  movieDetailsContainer: document.querySelector('.pop-up__container'),
 };
