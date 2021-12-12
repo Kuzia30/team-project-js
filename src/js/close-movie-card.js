747f1fca07aeffcf0b3a3c351f4995afd41ca4d5
Yakov <yakoventii@gmail.com>
2021-12-12 17:47:08 +0200

    fix video close

--- src/js/close-movie-card.js
+++ src/js/close-movie-card.js
@@ -7,18 +7,19 @@ export function closeMovieWindow() {
   refs.closeBtnMobile.removeEventListener('click', closeMovieWindow);
   window.removeEventListener('keydown', closeEscape);
   refs.aboutFilmWindow.removeEventListener('click', closeBackdrop);
-  
+  offVideo();
 }
 
 export function closeEscape(event) {
   if (event.key === 'Escape') {
+    offVideo();
     refs.backdrop.classList.add('visually-hidden');
     refs.body.classList.remove('overflow-hidden');
     window.removeEventListener('keydown', closeEscape);
     refs.aboutFilmWindow.removeEventListener('click', closeBackdrop);
     refs.closeBtn.removeEventListener('click', closeMovieWindow);
     refs.closeBtnMobile.removeEventListener('click', closeMovieWindow);
-      return;
+    return;
   }
 }
 
@@ -26,6 +27,7 @@ export function closeBackdrop(event) {
   if (event.currentTarget !== event.target) {
     return;
   } else {
+    offVideo();
     refs.backdrop.classList.add('visually-hidden');
     refs.body.classList.remove('overflow-hidden');
     refs.aboutFilmWindow.removeEventListener('click', closeBackdrop);
@@ -33,14 +35,17 @@ export function closeBackdrop(event) {
     refs.closeBtn.removeEventListener('click', closeMovieWindow);
     refs.closeBtnMobile.removeEventListener('click', closeMovieWindow);
   }
-  
 }
 
-  // if (refs.watchedBtn.classList.contains('library__btn' && 'current-btn' && 'watched-js') === true) {
-    //   // showWatchedFilms();
-    //   console.log('SHOWWHATHED');
-    // }
-    // if (refs.queueBtn.classList.contains('library__btn' && 'queue-js' && 'current-btn') === true) {
-    //   // showQueue();
-    //   console.log('SHOWWQUEUE');
-    // }
+function offVideo() {
+  const trailerEl = (refs.movieDetailsContainer.querySelector('.trailer').innerHTML = '');
+}
+
+// if (refs.watchedBtn.classList.contains('library__btn' && 'current-btn' && 'watched-js') === true) {
+//   // showWatchedFilms();
+//   console.log('SHOWWHATHED');
+// }
+// if (refs.queueBtn.classList.contains('library__btn' && 'queue-js' && 'current-btn') === true) {
+//   // showQueue();
+//   console.log('SHOWWQUEUE');
+// }
